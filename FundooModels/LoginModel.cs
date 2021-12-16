e9271dbcd98dcdb3ce63c8f9ac66990900aa4a76
siba98 <gyansiba.patro@gmail.com>
2021-12-16 18:49:09 +0530

    [Siba]:Refactored Files  For User

--- FundooModels/LoginModel.cs
+++ FundooModels/LoginModel.cs
@@ -7,12 +7,12 @@ namespace FundooModels
 {
     public class LoginModel
     {
-        //[Required]
-        //[RegularExpression("^[a-zA-Z0-9]+([.#_$+-][a-zA-Z0-9]+)*[@][a-zA-Z0-9]+[.][a-zA-Z]{2,3}([.][a-zA-Z]{2})?$", ErrorMessage = "Email is not valid. Please Enter valid email")]
+        [Required]
+        [RegularExpression("^[a-zA-Z0-9]+([.#_$+-][a-zA-Z0-9]+)*[@][a-zA-Z0-9]+[.][a-zA-Z]{2,3}([.][a-zA-Z]{2})?$", ErrorMessage = "Email is not valid. Please Enter valid email")]
         public string Email { get; set; }
 
-        //[Required]
-        //[RegularExpression("^(?=.?[A-Z])(?=.?[a-z])(?=.?[0-9])(?=.?[#?!@$ %^&*-]).{8,}$", ErrorMessage = "Password is not valid. Password Should be 8 Character contain 1 Uppercase, 1 Special character, 1 Number")]
+        [Required]
+        [RegularExpression("^(?=.?[A-Z])(?=.?[a-z])(?=.?[0-9])(?=.?[#?!@$ %^&*-]).{8,}$", ErrorMessage = "Password is not valid. Password Should be 8 Character contain 1 Uppercase, 1 Special character, 1 Number")]
         public string Password { get; set; }
     }
 }
--- FundooRepository/Context/UserContext.cs
+++ FundooRepository/Context/UserContext.cs
@@ -14,8 +14,6 @@ namespace FundooRepository.Context
         }
 
         public DbSet<RegisterModel> Users { get; set; }
-        public DbSet<NoteModel> Note { get; set; }
-        public DbSet<CollaboratorModel> Collaborator { get; set; }
 
     }
 }
