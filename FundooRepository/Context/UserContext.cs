64c445bf00eabd217ba1a1e5245815dd68e98a82
siba98 <gyansiba.patro@gmail.com>
2021-12-16 18:54:52 +0530

    [Siba]:Refactored Files For Note Api's

--- FundooRepository/Context/UserContext.cs
+++ FundooRepository/Context/UserContext.cs
@@ -15,7 +15,6 @@ namespace FundooRepository.Context
 
         public DbSet<RegisterModel> Users { get; set; }
         public DbSet<NoteModel> Note { get; set; }
-        public DbSet<CollaboratorModel> Collaborator { get; set; }
 
     }
 }
